aaab9d7622f8814cadad1656a43085ea3a165f0d
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2021-03-12 07:06:51 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Song
+  attr_accessor :title
+ 
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end 
+    binding.pry 
+    
+  end 
---
+++ looping.rb
@@ -0,0 +1,6 @@
+toppings = ["onions", "pickles", "mushrooms", "bacon"]
+def hamburger(toppings)
+  toppings.collect do |topping|
+    "I love #{topping} on my burgers"
+  end
+end
